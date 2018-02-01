a74e27f8951397091e59d26a464a034971d96a78
Chris <zklhp001@gmail.com>
2018-02-01 16:06:41 +0800

    Always mkdir artifacts

--- ci-build.sh
+++ ci-build.sh
@@ -7,7 +7,7 @@
 # Configure
 cd "$(dirname "$0")"
 source 'ci-library.sh'
-deploy_enabled && mkdir artifacts
+mkdir artifacts
 git_config user.email 'ci@msys2.org'
 git_config user.name  'MSYS2 Continuous Integration'
 git remote add upstream 'https://github.com/Alexpux/MSYS2-packages'
